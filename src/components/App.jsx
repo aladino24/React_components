00872fcf070c4d6ca5a60d0984ad0fcd4a945363
Aladino Zulmar Abadi <74063303+aladino24@users.noreply.github.com>
2021-12-28 10:53:40 +0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Box from "./Box";
+import List from "./List";
+
+function App() {
+  return (
+    <div>
+      <Box />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Box.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Box() {
+  return <h1>My Favourite Foods</h1>;
+}
+
+export default Box;
